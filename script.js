8054c2a7fee8471de0de4a7d742b646bc9da802a
rusty-reebs <rusty_reebs@tutanota.com>
2021-07-23 11:26:01 +0400

    created cells array and event listener in gameBoard module

--- script.js
+++ script.js
@@ -4,36 +4,56 @@
 
 //! Goal -- use as little global code as possible. Use factories and modules.
 
-// DOM objects to be manipulated
-const cell1 = document.getElementById("one"); 
-const cell2 = document.getElementById("two");
-const cell3 = document.getElementById("three");
-const cell4 = document.getElementById("four");
-const cell5 = document.getElementById("five");
-const cell6 = document.getElementById("six");
-const cell7 = document.getElementById("seven");
-const cell8 = document.getElementById("eight");
-const cell9 = document.getElementById("nine");
 
 // Module for gameboard object and array
 const gameBoard = (() => {
+    // DOM objects to be manipulated
+    const celldivs = document.querySelectorAll(".cell"); // squares is a NodeList, not an array
+    const cells = Array.from(celldivs);
+    
+    cells.forEach(cell => {
+        cell.addEventListener("click", () => {  // This adds click listener to each cell
+            console.log(cell);
+            })
+        });
+
     const gameArray = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
     
     const populateBoard = () => {
-        cell1.innerHTML = gameArray[0];
-        cell2.innerHTML = gameArray[1];
-        cell3.innerHTML = gameArray[2];
-        cell4.innerHTML = gameArray[3];
-        cell5.innerHTML = gameArray[4];
-        cell6.innerHTML = gameArray[5];
-        cell7.innerHTML = gameArray[6];
-        cell8.innerHTML = gameArray[7];
-        cell9.innerHTML = gameArray[8];
+        // cells.forEach(index => {
+        //     cells[index].innerHTML = gameArray[index];  //! This didn't work
+        // });
+
+        cells[0].innerHTML = gameArray[0];  // but this does
+        cells[1].innerHTML = gameArray[1];
+        cells[2].innerHTML = gameArray[2];
+        cells[3].innerHTML = gameArray[3];
+        cells[4].innerHTML = gameArray[4];
+        cells[5].innerHTML = gameArray[5];
+        cells[6].innerHTML = gameArray[6];
+        cells[7].innerHTML = gameArray[7];
+        cells[8].innerHTML = gameArray[8];
     };
 
-return {gameArray, populateBoard};
-
+return {populateBoard, gameArray, cells}; // makes methods public
 })();
+
+
+// Module for game flow
+
+//! decide what to put in game flow module
+// TODO upon click, then change array to X or O
+
+
+// This code works to change the first cell
+// cell1.addEventListener("click", () => {
+    //     gameBoard.gameArray.splice(0, 1, "A");
+    //     console.log(gameBoard.gameArray);
+    //     gameBoard.populateBoard();
+    // })
+    
+gameBoard.populateBoard();
+
 
 // Factory for players
 
@@ -41,16 +61,3 @@ const player = (playername) => {
 
     console.log(playername);
 };
-
-// Module for game flow
-
-// TODO event listener for clicks on cell divs
-// TODO upon click, then change array to X or O
-
-cell1.addEventListener("click", () => {
-    gameBoard.gameArray.splice(0, 1, "A");
-    console.log(gameBoard.gameArray);
-    gameBoard.populateBoard();
-})
-
-gameBoard.populateBoard();
