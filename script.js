bb639a75cfc7a3e27d43cfb3376a3bb3c3666468
rusty-reebs <rusty_reebs@tutanota.com>
2021-08-05 09:59:57 +0400

    completed form submit function. Project fully operational.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 //! Goal -- use as little global code as possible. Use factories and modules.
 
-//TODO stop cell clicks during name inputs
 
 // Game Array Method -- holds the array of X's and O's
 const gameArray = (() => {
     const newArray = ["", "", "", "", "", "", "", "", ""];
-
     return { newArray };
 })();
+
 
 // Game Board Method -- fills in the cells with the array
 const gameBoard = (() => {
@@ -16,17 +15,18 @@ const gameBoard = (() => {
     const populateBoard = () => {
         cells.forEach((cell, index) => {  // fills in cells with array items
             cells[index].innerHTML = gameArray.newArray[index];
-        })
+        });
     };
     return { populateBoard, cells };
 })();
+
 
 // Game Play Method -- adds markers and checks for tie and win
 const playGame = (() => {
     let winner = null;
     let move = 1;
     gameBoard.cells.forEach((cell, index) => {
-        cell.addEventListener("click", () => {  // This adds click listener to each cell
+        cell.addEventListener("click", () => {  // adds click listener to each cell
             console.log(cell);
             if (checkCell(index)) {
                 console.log("Occupado");
@@ -61,9 +61,9 @@ const playGame = (() => {
                     openPop("Tie Game!");
                     setTimeout(() => gameInit(), 2000);
                     console.log(gameArray.newArray);
-                };
+                }
             }
-            })
+            });
         });
     
     const checkforWin = () => {  // function to check for winner
@@ -78,11 +78,11 @@ const playGame = (() => {
             [2, 4, 6]
             ];
 
-        winCombos.forEach((combo, index) => {
+        winCombos.forEach((combo, index) => {   // runs function on each winning combo
             if (gameArray.newArray[combo[0]] && gameArray.newArray[combo[0]] === gameArray.newArray[combo[1]] && gameArray.newArray[combo[0]] === gameArray.newArray[combo[2]]) {
                 // winner = gameArray.newArray[combo[0]];
                 winner = true;
-            };
+            }
             return winner;
         });
     };
@@ -93,7 +93,7 @@ const playGame = (() => {
             openPop("Square Taken!");
             setTimeout(() => closePop(), 2000);
             return true;
-        };
+        }
     };
 
     const checkForTie = () => {  // function to check for a tie
@@ -102,35 +102,38 @@ const playGame = (() => {
             // openPop("Tie Game!");
             // setTimeout(() => closePop(), 2000);
             return true;
-        };
+        }
     };
     
-    //TODO add a button New Players
-    const getPlayerNames = () => {
+    const openPlayerNames = () => {
         document.getElementById("namespopup").style.display = "block";
-        
-    }
+    };
     
     const closePlayerNames = () => {
         document.getElementById("namespopup").style.display = "none";
-    }
+    };
     
-    const restart = document.getElementById("restart");
+    const restart = document.getElementById("restart");     // Restart button
     restart.addEventListener("click", () => {
         console.log("reset");
         playGame.gameInit();
-    }
-    );
+    });
+
+    const newPlayers = document.getElementById("newplayers");   // New Players button
+    newPlayers.addEventListener("click", () => {
+        console.log("new players");
+        playGame.gameInit();        // clears game board
+        player1Name.value = "";     // resets player names on popup form
+        player2Name.value = "";
+        openPlayerNames();           // opens popup form
+    });
     
     const gameInit = () => {  // initializes variables, arrays and game
-        //! resets array and calls populateBoard
         closePop();
         winner = null;
         move = 1;
         gameArray.newArray = ["", "", "", "", "", "", "", "", ""];
         gameBoard.populateBoard();
-        // getPlayerNames();  // don't want to reset player names on new game
-        
     };
 
     const openPop = (text) => {      // multipurpose pop up box
@@ -142,30 +145,24 @@ const playGame = (() => {
         document.getElementById("popup").style.display = "none";
     };
 
-    
-    //TODO need local storage
-
-
-    return { gameInit, getPlayerNames, closePlayerNames };
+    return { gameInit, openPlayerNames, closePlayerNames };
     
 })();
 
 
-// TODO get player name form submit working
-// const form = document.getElementById("form");
-// const player1 = "";
-// const player2 = "";
-// const player1Name = document.getElementById("player1");
-// const player2Name = document.getElementById("player2");
-// const submitNames = () => {
-//     player1 = Player (player1Name.value, "X");
-//     player2 = Player (player2Name.value, "O");
-//     setTimeout(closePlayerNames(), 300);
-// };
+const form = document.querySelector("form");
+const player1Name = document.getElementById("player1");
+const player2Name = document.getElementById("player2");
+const submitNames = () => {
+    player1 = Player (player1Name.value, "X");
+    player2 = Player (player2Name.value, "O");
+    setTimeout(playGame.closePlayerNames(), 300);
+    playGame.gameInit();
+};
 
-// form.addEventListener("submit", function (e) { // stops refresh
-//     e.preventDefault();
-// });
+form.addEventListener("submit", function (e) {      // stops refresh on submit
+    e.preventDefault();
+});
 
 
 // TODO we want objects to describe our players and encapsulate all of the things our players can do (functions!)
@@ -181,10 +178,10 @@ const Player = (name, marker) => {
     return { getName, go };
 };
 
-player1 = Player ("Rusty", "X");
-player2 = Player ("Jaime", "O");
+// player1 = Player ("Rusty", "X");
+// player2 = Player ("Jaime", "O");
+// const nameArray = [];
 
 gameBoard.populateBoard();
-// playGame.getPlayerNames();
-
+playGame.openPlayerNames();
 
